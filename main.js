218ab8ab024e3ba48812d88bacdf35d09fa95ac9
David <deivi_gf144@hotmail.com>
2023-01-24 22:54:51 -0400

    :sparkles: Finish the BoardGame object

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ import "./src/styles/elements/base.css";
 import BoardGame from "./src/objects/BoardGame";
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(10);
+const $htmlBoardGame = BoardGame(6);
 $root.insertAdjacentHTML("beforeend", $htmlBoardGame);
